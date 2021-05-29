652b455f1c9dcee6ff34be39c3c89ca51bda3177
kohta1208 <kohtafms@gmail.com>
2021-05-29 23:55:00 +0900

    Place two buttons
    ボタンを二つ配置する

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,43 @@
 <template>
-  <div id="home">
-    hello
+  <div id="home" class="d-flex">
+    <!-- 左のカード -->
+    <v-card
+      tile
+      :width="width"
+      :height="height"
+      :style="{
+        position:`absolute`,
+        top:`50%`,
+        left:`50%`,
+        transform:`translate(-100%,-50%) translateX(-${distance/2}px)`
+      }"
+    ></v-card>
+    <!-- 右のカード -->
+    <v-card
+      tile
+      :width="width"
+      :height="height"
+      :style="{
+        position:`absolute`,
+        top:`50%`,
+        left:`50%`,
+        transform:`translate(0%,-50%) translateX(${distance/2}px)`
+      }"
+    ></v-card>
+
   </div>
 </template>
 
 <script>
   export default {
     name: 'Home',
+    data() {
+      return {
+        distance:100,
+        width:100,
+        height:100,
+      }
+    },
 
     components: {
     },
