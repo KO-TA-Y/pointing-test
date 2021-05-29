64ebfb4e51b4506ff77e4e660732cc82461d321f
kohta1208 <kohtafms@gmail.com>
2021-05-29 21:07:49 +0900

    clear default component
    デフォルトのコンポーネントを削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,14 @@
 <template>
-  <hello-world />
+  <div>
+    hello
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
     },
   }
 </script>
