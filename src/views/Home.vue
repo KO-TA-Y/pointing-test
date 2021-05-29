60532e44fe61d22bdf2df45b5694065638923c4f
kohta1208 <kohtafms@gmail.com>
2021-05-30 03:20:19 +0900

    Turn each button red alternately
    ボタンを交互に赤にする

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="home" class="d-flex">
+    <div class="pl-7 pt-7">
+      {{ num }}/{{ total }}セット目<br />
+      成功回数：{{ success }}/{{ totalSuccess }}<br />
+      ミス回数：{{ miss }}
+    </div>
     <!-- 左のカード -->
     <v-card
+      @mousedown="incrementSuccess"
       tile
       :width="width"
       :height="height"
+      :color="
+            success % 2 == 0 && success < totalSuccess
+              ? targetColor
+              : disabledColor
+          "
+      :disabled="
+            (success % 2 == 0) && (success < totalSuccess)
+              ? false
+              : true
+          "
       :style="{
         position:`absolute`,
         top:`50%`,
@@ -14,9 +30,20 @@
     ></v-card>
     <!-- 右のカード -->
     <v-card
+      @mousedown="incrementSuccess"
       tile
       :width="width"
       :height="height"
+      :color="
+            success % 2 == 0 || success >= totalSuccess
+              ? disabledColor
+              : targetColor
+          "
+      :disabled="
+            success % 2 == 0 || success >= totalSuccess
+              ? true
+              : false
+          "
       :style="{
         position:`absolute`,
         top:`50%`,
@@ -36,9 +63,21 @@
         distance:100,
         width:100,
         height:100,
+        num:1,
+        total:36,
+        totalSuccess:7,
+        success:0, //ボタンを押したら増えて、偶奇で色などを変える
+        miss:0,
+        targetColor: "red",
+        disabledColor: "white",
       }
     },
 
+    methods: {
+      incrementSuccess(){
+        this.success++
+      },
+    },
     components: {
     },
   }
